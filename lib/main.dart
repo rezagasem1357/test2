73385fa08da9207112a8a622119aa9d93890f896
rezagasem1357 <rezagasem.82@gmail.com>
2026-08-21 17:19:28 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class DeliveryApp extends StatelessWidget {
           elevation: 0,
           centerTitle: true,
         ),
-        cardTheme: CardThemeData(
+        cardTheme: const CardTheme(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
