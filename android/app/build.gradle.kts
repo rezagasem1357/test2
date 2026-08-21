51912b5254107ee0439feda34881fa832a60b829
rezagasem1357 <rezagasem.82@gmail.com>
2026-08-21 12:13:12 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,9 @@ plugins {
 
 android {
     namespace = "com.example.flutter_application_3"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
